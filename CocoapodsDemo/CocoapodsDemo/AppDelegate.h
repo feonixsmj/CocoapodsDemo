65d14fd93bfacaa7f36190dfedc9050371372bdd
suminjie <suminjie@rd.tuan800.com>
2017-02-27 10:28:00 +0800

    test

---
+++ CocoapodsDemo/CocoapodsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoapodsDemo
+//
+//  Created by suminjie on 17/2/27.
+//  Copyright © 2017年 feonixsmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoapodsDemo/CocoapodsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoapodsDemo
+//
+//  Created by suminjie on 17/2/27.
+//  Copyright © 2017年 feonixsmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
